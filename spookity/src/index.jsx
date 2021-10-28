9a56720430e4098b9e7ae1e5fe23ba96ba2b418a
Holben888 <bholmesdev@gmail.com>
2021-10-28 18:54:28 -0400

    👻 initial cooOOoomit

---
+++ spookity/src/index.jsx
@@ -0,0 +1,10 @@
+export const frontMatter = {
+  title: 'Heeeeeres Slinkity!',
+  layout: 'layout',
+}
+
+const HeeeeeeresSlinkity = ({  }) => (
+  <h1 className="text-xl p-3">{frontMatter.title}</h1>
+)
+
+export default HeeeeeeresSlinkity
