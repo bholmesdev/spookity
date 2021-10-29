ed4c39c9086335d293f8b00ff1c7bc30db4925c3
Holben888 <bholmesdev@gmail.com>
2021-10-28 22:23:12 -0400

    refactor: add useLocalStorage util

---
+++ spookity/utils/useLocalStorage.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+// Hook
+export function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof(window) === 'undefined') return initialValue
+    try {
+      // Get from local storage by key
+      const item = window?.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+      // Parse stored json or if none return initialValue
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    if (typeof(window) === 'undefined') return initialValue
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
